39e4c8ca29183286ff6b3fba19baf31e4aedcd16
ManiacViper <kenneth.d.monteiro@gmail.com>
2015-09-03 20:25:55 +0100

    added profanity api

--- src/main-urban-dict.py
+++ src/main-urban-dict.py
@@ -1,41 +1,46 @@
+import prof
 import requests, json
 
 URBANDICT_TERM_URL = "http://api.urbandictionary.com/v0/define?term="
 term = ''
 
 # commands
-def show_related_terms():
-    response = get_term()
+def show_related_terms(term):
+    response = get_term(term)
     tags = ''
     for tag in response['tags']:
         tags = tags + (', ' if tags != '' else '') + tag
-    print('tags')
-    print(tags)
+    prof.cons_show('tags')
+    prof.cons_show(tags)
 
-def show_term(size):
-    response = get_term()
+def show_term(size, term):
+    response = get_term(term)
     count = 1
     for definition in response['list'][:size]:
-        print('')
-        print(str(count) + ')')
-        print('Definition:')
-        print(definition['definition'])
-        print('')
-        print('Example:')
-        print(definition['example'])
+        prof.cons_show('')
+        prof.cons_show(str(count) + ')')
+        prof.cons_show('Definition:')
+        prof.cons_show(definition['definition'])
+        prof.cons_show('')
+        prof.cons_show('Example:')
+        prof.cons_show(definition['example'])
         count += 1
 
 # helpers
-def get_term():
-    global term
-    if term == '':
-        term = raw_input('search term: ')
+def get_term(term):
     try:
         return requests.get(URBANDICT_TERM_URL + term).json()
     except requests.HTTPError as httpe:
-        print('cannot retrieve term')
-        print('error response' + httpe.response)
+        prof.cons_show('cannot retrieve term')
+        prof.cons_show('error response' + httpe.response)
 
-# execute
-show_related_terms()
-show_term(5)
+def execute(term) :
+    show_related_terms(term)
+    show_term(2, term)
+
+def prof_init(version, status):
+    prof.register_command("/urban", 1, 1,
+                          "/urban [word]",
+                          "display urban definitions",
+                          "display urban definitions",
+                          execute)
